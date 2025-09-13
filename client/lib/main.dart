2a09a308a8fa60041c2fbb5ee808e75732f3be5b
nirpechuk <npechuk@gmail.com>
2025-09-13 14:54:15 -0400

    env file

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/map_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await dotenv.load(fileName: ".env");
 
   await Supabase.initialize(
-    url: 'https://qypxekyjsdzbobmirznk.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5cHhla3lqc2R6Ym9ibWlyem5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3MjI2MzMsImV4cCI6MjA3MzI5ODYzM30.tRoAZvFJp8W5r5vy5AwXlggEkljPpvt0_-T4i6OmnZE',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
   runApp(const EcoActionApp());
 }
